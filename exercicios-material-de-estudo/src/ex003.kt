cd635d0f01cb2c2a7d02bdac19f39b8fc6bda928
Douglas Seramim <douglasfseramim@gmail.com>
2025-04-13 18:54:10 -0300

    Exercício 3 parte 2

--- exercicios-material-de-estudo/src/ex003.kt
+++ exercicios-material-de-estudo/src/ex003.kt
@@ -1,3 +1,5 @@
+import kotlin.text.replace
+
 fun main(){
     exibirMenu()
 }
@@ -15,7 +17,7 @@ private fun exibirMenu() {
 
         when (opcao.toInt()) {
             1 -> somarNumerosReais()
-            2 -> {}
+            2 -> gerarNumeroAleatorio()
             3 -> {}
             4 -> println("Saindo...")
         }
@@ -23,82 +25,97 @@ private fun exibirMenu() {
     } while (opcao != 4)
 }
 
-private fun getOpcao(): Int {
-    val regexOp = Regex("^[1-4]\$")
-    var opcao = ""
+private fun gerarNumeroAleatorio() {
+    println("# Gerar número aleatório #")
+    var inicio: Int = 0
+    var fim: Int = 0
+
     do {
-        print("Insira uma opção: ")
-        opcao = readln().toString()
-        var valida: Boolean = regexOp.matches(opcao)
+        inicio = getInt("Insira o número inicial: ")
+        fim = getInt("Insira o número final: ")
 
-        if (!valida) {
-            println("Opção inválida!")
+        if (inicio > fim) {
+            println("O número final deve ser maior que o número inicial! Tente novamente.")
         }
-    } while (!valida)
+    } while (inicio > fim)
 
-    if (opcao.toIntOrNull() == null) {
-        return 0
-    }
-    return opcao.toInt()
+    var resultado = (inicio..fim).random()
+    println("Número gerado: $resultado")
+
+    print("Pressione ENTER para voltar ao menu")
+    readln().toString()
 }
 
-fun somarNumerosReais() {
-    var quanti = getQuanti()
+private fun somarNumerosReais() {
+    var quanti = getInt("Informe quantos números deseja informar (de 1 até 99): ", Regex("^[1-9][0-9]?\$"))
     var lista = mutableListOf<Float>()
     montarLista(quanti, lista)
 
     var soma = 0F
+    var resultado = ""
 
-    for (numero in lista) {
-        soma += numero
+    for (i in 0..((quanti.toInt()) - 1)) {
+        soma += lista[i]
+        if (i == 0) {
+            resultado = lista[i].toString()
+            continue
+        }
+        resultado += " + " + lista[i]
     }
 
-    println("Resultado da soma: $soma")
+    println("# Resultado da soma #")
+    println("$resultado: $soma")
+
+    print("Pressione ENTER para voltar ao menu")
+    readln().toString()
 }
 
-private fun getQuanti(): String {
-    // # Anotações sobre a expressão regular #
-    // ^ -> marca o início da string
-    // [1-9] -> indica que o primeiro dígito deve estar entre 1 e 9
-    // \\d* -> indica que a string pode ter 0 ou mais dígitos adicionais
-    // $ -> indica o fim da string
-    // val regex = Regex("^[1-9]\\d*\$")
-
-    // mudei para uma opção que limita a quantidade para o máximo de 99
-    // pq acho q faz mais sentido :)
-    // ? -> indica que o segundo dígito é opcional
-
-    val regex = Regex("^[1-9][0-9]?\$")
-    var quanti = ""
+// # Anotações sobre a expressão regular #
+// ^ -> marca o início da string
+// [1-9] -> indica que o primeiro dígito deve estar entre 1 e 9
+// \\d* -> indica que a string pode ter 0 ou mais dígitos adicionais
+// $ -> indica o fim da string
+//"^[1-9]\\d*\$"
+private fun getInt(mensagem: String = "Informe um número: ",
+                      regex: Regex = Regex("^-?\\d+\$"),
+                   msgInvalida: String = "Número inválido!"): Int {
+    var numero = ""
     do {
-        print("Informe quantos números deseja informar (de 1 até 99): ")
-        quanti = readln().toString()
-        var valida: Boolean = regex.matches(quanti)
+        print(mensagem)
+        numero = readln().toString()
+        var valida: Boolean = regex.matches(numero)
+
+        if (!valida) {
+            println(msgInvalida)
+        }
+    } while (!valida)
+    return numero.toInt()
+}
+
+private fun montarLista(quanti: Int, lista: MutableList<Float>) {
+    for (i in 1..quanti) {
+        lista.add(getFloat("Número $i: "))
+    }
+}
+
+private fun getFloat(mensagem: String = "Informe um número: "): Float {
+    var numero = ""
+    do {
+        print(mensagem)
+        numero = readln().toString()
+        numero = numero.replace(',', '.')
+        var valida: Boolean = validaNumero(numero)
 
         if (!valida) {
             println("Opção inválida!")
         }
     } while (!valida)
-    return quanti
+    return numero.toFloat()
 }
-
-private fun montarLista(quanti: String, lista: MutableList<Float>) {
-    for (i in 1..quanti.toInt()) {
-        do {
-            print("Número $i: ")
-            var input = readln().toString()
-            input = input.replace(',', '.')
-
-            var valida: Boolean = validaNumero(input)
-            if (!valida) {
-                println("Número inválido!")
-                continue
-            }
-            lista.add(input.toFloat())
-        } while (!valida)
-    }
-}
-
-fun validaNumero(input: String): Boolean {
+private fun validaNumero(input: String): Boolean {
     return input.toFloatOrNull() != null
+}
+
+private fun getOpcao(): Int {
+    return getInt("Insira uma opção: ", Regex("^[1-4]\$"), "Opção inválida!")
 }
