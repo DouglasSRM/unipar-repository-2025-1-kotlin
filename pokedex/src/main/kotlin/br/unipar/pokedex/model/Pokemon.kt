4ee53f7e1eed498bde55cfb3ed6356a6e014e7b1
Douglas Seramim <douglasfseramim@gmail.com>
2025-04-16 20:41:15 -0300

    update

--- pokedex/src/main/kotlin/br/unipar/pokedex/model/Pokemon.kt
+++ pokedex/src/main/kotlin/br/unipar/pokedex/model/Pokemon.kt
@@ -2,17 +2,23 @@ package br.unipar.pokedex.model
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PositiveOrZero
 
 @Entity
 class Pokemon (
     @Id
-    var id: Int,
+    var numeroPokedex: Int,
 
+    @NotBlank
     var nome: String,
 
+    @NotBlank
     var tipo_1: Tipo,
 
     var tipo_2: Tipo?,
 
+    @PositiveOrZero
     var poder: Int
 )
